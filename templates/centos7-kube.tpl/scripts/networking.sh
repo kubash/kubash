71028feca02dc2c5c9a45421266af7b2391b01dd
Josh Cox <josh@webhosting.coop>
2020-08-02 13:32:02 -0500

    http

--- templates/centos7-kube.tpl/scripts/networking.sh
+++ templates/centos7-kube.tpl/scripts/networking.sh
@@ -10,8 +10,17 @@ virtualbox-iso|virtualbox-ovf)
         # Add 'single-request-reopen' so it is included when /etc/resolv.conf is
         # generated
         # https://access.redhat.com/site/solutions/58625 (subscription required)
+
         echo 'RES_OPTIONS="single-request-reopen"' >>/etc/sysconfig/network;
-        service network restart;
+
+        if [ "$major_version" -ge 8 ]; then
+            nmcli networking off
+            sleep 5
+            nmcli networking on
+        else
+            service network restart;
+        fi
+
         echo 'Slow DNS fix applied (single-request-reopen)';
     fi
     ;;
