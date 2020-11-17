355d1bdd3fd125389da3e7d658cf784eb54a9fe9
Josh Cox <josh@webhosting.coop>
2020-11-17 11:13:46 -0600

    updateSubmodules

--- lib/istio.bash
+++ lib/istio.bash
@@ -15,11 +15,12 @@ EOF
     KUBECONFIG=$KUBECONFIG \
     istioctl install \
       --set profile=$ISTIO_PROFILE \
-      --set values.kiali.enabled=true \
-      --set values.tracing.enabled=true \
       --set "values.kiali.dashboard.jaegerURL=http://jaeger-query:16686" \
       --set "values.global.tracer.zipkin.address=jaeger-collector:9411" \
       --set "values.kiali.dashboard.grafanaURL=http://grafana:3000"
+      # deprecated
+      #--set values.kiali.enabled=true \
+      #--set values.tracing.enabled=true \
 
     KUBECONFIG=$KUBECONFIG \
     kubectl label namespace default --overwrite istio-injection=enabled
