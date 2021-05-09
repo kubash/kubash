54d49b43b851f34585fec8e3383927b53b2f48e2
Josh Cox <josh@webhosting.coop>
2021-05-09 14:11:32 -0500

    lvm

--- lib/storage.bash
+++ lib/storage.bash
@@ -5,7 +5,7 @@ mount_all_iscsi_targets () {
   while IFS="," read -r $csv_columns
   do
     squawk 185 "ROLE $K8S_role $K8S_user $K8S_ip1 $K8S_sshPort"
-    squawk 3 "initializing storage node $@"
+    squawk 3 "initializing iscsi node $@"
     squawk 33 "${K8S_iscsihost} ${K8S_iscsitarget} $K8S_iscsichapusername"
     if [[ "$K8S_iscsitarget" != "null" ]]; then
       squawk 3 "K8S_iscsitarget=$K8S_iscsitarget"
@@ -31,6 +31,22 @@ mount_all_iscsi_targets () {
   done <<< "$kubash_hosts_csv_slurped"
 }
 
+check_first_device () {
+  if [[ $1  == "vda" || $1  == "sda" || $1  == "hda" ]]; then
+    croak 0 "WARNING! Formatting the first device is most likely a bad idea \n Open a support request at https://github.com/kubash/kubash/issues/new"
+  fi
+}
+
+lvm_creation_run () {
+  THIS_storageTarget=$1
+  THIS_sshPort=$2
+  THIS_user=$3
+  THIS_ip1=$4
+  check_first_device $THIS_storageTarget
+  command2run="blkid /dev/${THIS_storageTarget} && pvcreate /dev/${THIS_storageTarget} && vgcreate /dev/${THIS_storageTarget}"
+  sudo_command "$THIS_sshPort" "$THIS_user" "$THIS_ip1" "$command2run"
+}
+
 mount_all_other_targets () {
   squawk 1 "mount all other targets $@"
   while IFS="," read -r $csv_columns
@@ -40,13 +56,16 @@ mount_all_other_targets () {
     squawk 3 "$K8S_storagePath $K8S_storageType $K8S_storageSize $K8S_storageTarget $K8S_storageMountPath $K8S_storageUUID"
 
     squawk 3 "${K8S_storagePath} ${K8S_storageType} ${K8S_storageSize} ${K8S_storageTarget} ${K8S_storageMountPath} ${K8S_storageUUID}"
-    if [[ ${K8S_storagePath} != "null" ]]; then
+    if [[ ${K8S_storagePath} == "lvm" ]]; then
+      squawk 3 "K8S_storagePath=$K8S_storagePath"
+      lvm_creation_run ${K8S_storageTarget} ${K8S_sshPort} ${K8S_user} ${K8S_ip1}
+    elif [[ ${K8S_storagePath} != "null" ]]; then
       squawk 3 "K8S_storagePath=$K8S_storagePath"
       if [[ ${K8S_storageMountPath} != "null" ]]; then
+        squawk 55 "K8S_storageMountPath=$K8S_storageMountPath"
         if [[ ${K8S_storageTarget}  != "null" ]]; then
-          if [[ ${K8S_storageTarget}  == "vda" || ${K8S_storageTarget}  == "sda" || ${K8S_storageTarget}  == "hda" ]]; then
-            croak 0 "WARNING! Formatting the first device is most likely a bad idea \n Open a support request at https://github.com/kubash/kubash/issues/new"
-          fi
+          squawk 44 "K8S_storageTarget=$K8S_storageTarget"
+          check_first_device $K8S_storageTarget
           command2run="mkdir -p $K8S_storageMountPath"
           sudo_command "$K8S_sshPort" "$K8S_user" "$K8S_ip1" "$command2run"
           if [[ ${K8S_storageUUID} != "null" ]]; then
@@ -79,7 +98,10 @@ mount_all_other_targets () {
     fi
 
     squawk 3 "${K8S_storagePath1} ${K8S_storageType1} ${K8S_storageSize1} ${K8S_storageTarget1} ${K8S_storageMountPath1} ${K8S_storageUUID1}"
-    if [[ ${K8S_storagePath1} != "null" ]]; then
+    if [[ ${K8S_storagePath1} == "lvm" ]]; then
+      squawk 3 "K8S_storagePath1=$K8S_storagePath1"
+      lvm_creation_run ${K8S_storageTarget1} ${K8S_sshPort} ${K8S_user} ${K8S_ip1}
+    elif [[ ${K8S_storagePath1} != "null" ]]; then
       squawk 3 "K8S_storagePath=$K8S_storagePath1"
       if [[ ${K8S_storageMountPath1} != "null" ]]; then
         if [[ ${K8S_storageTarget1}  != "null" ]]; then
@@ -118,7 +140,10 @@ mount_all_other_targets () {
     fi
 
     squawk 3 "${K8S_storagePath2} ${K8S_storageType2} ${K8S_storageSize2} ${K8S_storageTarget2} ${K8S_storageMountPath2} ${K8S_storageUUID2}"
-    if [[ ${K8S_storagePath2} != "null" ]]; then
+    if [[ ${K8S_storagePath2} == "lvm" ]]; then
+      squawk 3 "K8S_storagePath2=$K8S_storagePath2"
+      lvm_creation_run ${K8S_storageTarget2} ${K8S_sshPort} ${K8S_user} ${K8S_ip1}
+    elif [[ ${K8S_storagePath2} != "null" ]]; then
       squawk 3 "K8S_storagePath=$K8S_storagePath2"
       if [[ ${K8S_storageMountPath2} != "null" ]]; then
         if [[ ${K8S_storageTarget2}  != "null" ]]; then
@@ -157,7 +182,10 @@ mount_all_other_targets () {
     fi
 
     squawk 3 "${K8S_storagePath3} ${K8S_storageType3} ${K8S_storageSize3} ${K8S_storageTarget3} ${K8S_storageMountPath3} ${K8S_storageUUID3}"
-    if [[ ${K8S_storagePath3} != "null" ]]; then
+    if [[ ${K8S_storagePath3} == "lvm" ]]; then
+      squawk 3 "K8S_storagePath3=$K8S_storagePath3"
+      lvm_creation_run ${K8S_storageTarget3} ${K8S_sshPort} ${K8S_user} ${K8S_ip1}
+    elif [[ ${K8S_storagePath3} != "null" ]]; then
       squawk 3 "K8S_storagePath=$K8S_storagePath3"
       if [[ ${K8S_storageMountPath3} != "null" ]]; then
         if [[ ${K8S_storageTarget3}  != "null" ]]; then
@@ -204,7 +232,7 @@ BROKEN_mount_all_other_targets () {
   while IFS="," read -r $csv_columns
   do
     squawk 185 "ROLE $K8S_role $K8S_user $K8S_ip1 $K8S_sshPort"
-    squawk 3 "initializing storage node $@"
+    squawk 3 "initializing borken storage node $@"
     squawk 3 "$K8S_storagePath $K8S_storageType $K8S_storageSize $K8S_storageTarget $K8S_storageMountPath $K8S_storageUUID"
     for storage_iteration in {0..3}
     do
