69d2afc641ce879a8fbc625dcdf1557860102e2f
Josh Cox <josh@webhosting.coop>
2021-07-01 14:10:17 -0500

    fixes

--- scripts/test_bats.sh
+++ scripts/test_bats.sh
@@ -13,6 +13,7 @@ KUBASH_DIR=$1
 trap "rm -rvf $TMP" EXIT
 
 cp examples/example-cluster.yaml $TMP/
+cp -a .ci $TMP/
 cp ~/.kube/config $TMP/config
 
 cat <<EOF > $TMP/start.sh
@@ -21,7 +22,8 @@ set -ex
 export TERM=dumb
 kubash yaml2cluster /example-cluster.yaml -n test_one
 rm -Rf /root/.kubash/clusters/test_one
-bats /root/.kubash/.ci/.tests.bats
+#bats /root/.kubash/.ci/.tests.bats
+bats /ci/.tests.bats
 EOF
 chmod +x $TMP/start.sh
 
@@ -30,6 +32,7 @@ cat <<EOF > $TMP/Dockerfile
 FROM test_yaml2cluster
 RUN cd /root/.kubash; make bats
 COPY start.sh /start.sh
+COPY .ci /ci
 RUN mkdir -p /root/.kube
 COPY config /root/.kube/config
 ENTRYPOINT /start.sh
--- scripts/test_bootstrap.sh
+++ scripts/test_bootstrap.sh
@@ -2,16 +2,22 @@
 set -eux
 TMP=$(mktemp -d)
 RAND_TMP="$(uuidgen -r | sha256sum | base64 | head -c 8)"
-trap "rm -rvf $TMP" EXIT
+trap "rm -rf $TMP" EXIT
 
 cp bootstrap $TMP/
 cp templates/bashrc.tpl $TMP/.bashrc
+cd ..
+sudo cp -a .kubash $TMP/kubash
+sudo chown -R ${USER}. $TMP/kubash
+rm -Rf $TMP/kubash/submodules/*
+echo kubash/submodules > .dockerignore
 
 cat <<EOF > $TMP/Dockerfile
 FROM ubuntu:bionic
 ENV TERM=dumb
-COPY bootstrap /bootstrap
+COPY kubash /root/.kubash
 COPY .bashrc /root/.bashrc
+COPY bootstrap /bootstrap
 RUN bash /bootstrap -y
 EOF
 
