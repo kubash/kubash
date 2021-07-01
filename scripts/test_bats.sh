fb49e76c4a03ee1600a66fc2fd3d544584c6b80a
Josh Cox <josh@webhosting.coop>
2021-07-01 12:08:25 -0500

    test_bats.sh

---
+++ scripts/test_bats.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+  # Print usage
+  echo -n 'Error! wrong number of arguments'
+  echo " [$#]"
+  echo 'usage:'
+  echo "$0 KUBASH_DIR" 
+  exit 1
+fi
+TMP=$(mktemp -d)
+KUBASH_DIR=$1
+trap "rm -rvf $TMP" EXIT
+
+cp examples/example-cluster.yaml $TMP/
+cp ~/.kube/config $TMP/config
+
+cat <<EOF > $TMP/start.sh
+#!/bin/bash -l
+export TERM=dumb
+bats /root/.kubash/.ci/.tests.bats
+EOF
+chmod +x $TMP/start.sh
+
+
+cat <<EOF > $TMP/Dockerfile
+FROM test_yaml2cluster
+RUN cd /root/.kubash; make bats
+COPY start.sh /start.sh
+RUN mkdir -p /root/.kube
+COPY config /root/.kube/config
+ENTRYPOINT /start.sh
+EOF
+
+cd $TMP
+docker build . -t test_bats
+docker run -it -v $KUBASH_DIR:/root/.kubash test_bats
