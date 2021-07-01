9a31844e15619b81cabedfa5726e21478cff6c25
Josh Cox <josh@webhosting.coop>
2021-07-01 10:21:15 -0500

    dockerized
    Signed-off-by: Josh Cox <josh@webhosting.coop>

--- scripts/test_bootstrap.sh
+++ scripts/test_bootstrap.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 TMP=$(mktemp -d)
+RAND_TMP="$(uuidgen -r | sha256sum | base64 | head -c 8)"
 trap "rm -rvf $TMP" EXIT
 
 cp bootstrap $TMP/
+cp templates/bashrc.tpl $TMP/.bashrc
+
 cat <<EOF > $TMP/Dockerfile
 FROM ubuntu:bionic
 COPY bootstrap /bootstrap
+COPY .bashrc /root/.bashrc
 RUN bash /bootstrap -y
 EOF
+
 cd $TMP
 docker build . -t test_bootstrap
---
+++ scripts/test_yaml2cluster.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+TMP=$(mktemp -d)
+TEST_CLUSTER_NAME=$(uuidgen -r | sha256sum | base64 | head -c 8)
+trap "rm -rvf $TMP" EXIT
+
+cp examples/example-cluster.yaml $TMP/
+cp templates/test_yaml2cluster_answer $TMP/
+
+
+cat <<EOF > $TMP/Dockerfile
+FROM test_bootstrap
+COPY example-cluster.yaml /example-cluster.yaml
+COPY test_yaml2cluster_answer /test_yaml2cluster_answer  
+RUN /bin/bash -l -c "kubash yaml2cluster /example-cluster.yaml -n $TEST_CLUSTER_NAME"
+#RUN ls -alhR /root/.kubash/clusters ; cat /root/.kubash/clusters/$TEST_CLUSTER_NAME/provision.csv
+RUN diff /root/.kubash/clusters/$TEST_CLUSTER_NAME/provision.csv /test_yaml2cluster_answer 
+EOF
+
+cd $TMP
+time docker build . -t test_yaml2cluster
